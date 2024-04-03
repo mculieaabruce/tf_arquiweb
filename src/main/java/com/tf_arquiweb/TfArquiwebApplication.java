7b3dc225c61aea2b6c5941475b2dc0b232957f20
andrerios.cum <ariosnoda18@gmail.com>
2024-04-03 00:01:01 -0500

    SiSI

---
+++ src/main/java/com/tf_arquiweb/TfArquiwebApplication.java
@@ -0,0 +1,13 @@
+package com.tf_arquiweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfArquiwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TfArquiwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tf_arquiweb/TfArquiwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tf_arquiweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfArquiwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
