0d257775be4e77384a444059d1e86cb7ae738663
AlfredoPachecoV <164974758+AlfredoPachecoV@users.noreply.github.com>
2024-04-03 15:19:44 -0500

    Alfredo

--- src/main/java/dtos/horarioDTO.java
+++ src/main/java/dtos/horarioDTO.java
@@ -1,4 +1,44 @@
 package dtos;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class horarioDTO {
+    private int id;
+    private LocalTime hora_inicio;
+    private LocalTime hora_fin;
+    private LocalDate fecha;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalTime getHora_inicio() {
+        return hora_inicio;
+    }
+
+    public void setHora_inicio(LocalTime hora_inicio) {
+        this.hora_inicio = hora_inicio;
+    }
+
+    public LocalTime getHora_fin() {
+        return hora_fin;
+    }
+
+    public void setHora_fin(LocalTime hora_fin) {
+        this.hora_fin = hora_fin;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
 }
+
