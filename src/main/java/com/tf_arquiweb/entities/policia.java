7b139a057e43b45087d02afbd9c4d25197f86043
AlfredoPachecoV <164974758+AlfredoPachecoV@users.noreply.github.com>
2024-04-24 15:11:14 -0500

    Entity policia

--- src/main/java/com/tf_arquiweb/entities/policia.java
+++ src/main/java/com/tf_arquiweb/entities/policia.java
@@ -21,4 +21,63 @@ public class policia {
     @JoinColumn(name = "horario_id")
     private horario horario;
 
+    public policia(int id, String nombre, String apellido, String num_placa, Comisaria comisaria, com.tf_arquiweb.entities.horario horario) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.num_placa = num_placa;
+        this.comisaria = comisaria;
+        this.horario = horario;
+    }
+
+    public policia() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNum_placa() {
+        return num_placa;
+    }
+
+    public void setNum_placa(String num_placa) {
+        this.num_placa = num_placa;
+    }
+
+    public Comisaria getComisaria() {
+        return comisaria;
+    }
+
+    public void setComisaria(Comisaria comisaria) {
+        this.comisaria = comisaria;
+    }
+
+    public com.tf_arquiweb.entities.horario getHorario() {
+        return horario;
+    }
+
+    public void setHorario(com.tf_arquiweb.entities.horario horario) {
+        this.horario = horario;
+    }
 }
